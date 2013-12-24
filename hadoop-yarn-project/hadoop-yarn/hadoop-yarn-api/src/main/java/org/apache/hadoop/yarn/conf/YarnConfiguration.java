4db90ee83fb440a31e559c04dbfdcaa63fb48f6c
limin <limin@limin-THINK>
2013-12-23 18:06:24 -0800

    changing container size

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-api/src/main/java/org/apache/hadoop/yarn/conf/YarnConfiguration.java
@@ -112,8 +112,8 @@ public class YarnConfiguration extends Configuration {
     DEFAULT_RM_SCHEDULER_PORT;
 
   /** Miniumum request grant-able by the RM scheduler. */
-  public static final String RM_SCHEDULER_CAPACITYSCHEDYLER_SUPPORT=RM_PREFIX
-          +"capacityscheduler.differentcap.support"; //limin
+  public static final String RM_SCHEDULER_CAPACITYSCHEDYLER_SUPPORT=
+          RM_PREFIX +"capacityscheduler.differentcap.support"; //limin
   public static final String DEFAULT_RM_SCHEDULER_CAPACITYSCHEDYLER_SUPPORT="false";
   public static final String RM_SCHEDULER_CAPACITYSCHEDYLER_SUPPORT_YES="true";
   
